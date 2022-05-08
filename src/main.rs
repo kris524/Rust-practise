c10b1bcd91d85ace1733eeeae8209174594563c2
Kris <kristian_dilov@abv.bg>
2022-05-08 18:12:38 +0100

    Sublist progress

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 
 use rand::Rng;
-mod exercism1;
+// mod exercism1;
+
 
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let x: u8 = rng.gen();
-    let y: u32 = rng.gen_range(1..3);
-    println!("x = {}", x);
-    println!("y = {}", y);
-    exercism1::sublist_checker(&[1,2,3], &[4,5,6])
+    // let x: u8 = rng.gen();
+    // let y: u32 = rng.gen_range(1..3);
+    // println!("x = {}", x);
+    // println!("y = {}", y);
+    // exercism1::sublist_checker( &[1,2,3], &[1,2,3,4]);
 }
 
 
