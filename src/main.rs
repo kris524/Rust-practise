3ed0d385435f6204b8dc6504b92024a3baff5c6d
Kris <kristian_dilov@abv.bg>
2022-05-18 23:31:11 +0100

    ex_health_stats_2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 use rand::Rng;
 // mod exercism1;
-
+#[derive(Debug)]
 pub struct User {
     name: String,
     age: u32,
@@ -11,6 +11,21 @@ pub struct User {
 impl User {
     pub fn new(name: String, age: u32, weight: f32) -> Self {
         println!("a User with name {}, age {}, and weight {}", name, age, weight)
+    }
+
+    pub fn name(&self) -> &str {
+        return &self.name;
+    }
+    pub fn age(&self) -> u32 {
+        return self.age;
+    }
+    pub fn weight(&self) -> f32 {
+        return self.weight;
+    }
+
+    pub fn set_age(&mut self, new_age: u32) {
+        self.age = new_age;
+        
     }
 }
 
@@ -26,7 +41,7 @@ fn main() {
     println!("{:?}", vector);
 
     let macro_vector = vec![5,3,6,3,55,2];
-    println!("{:?}", macro_vector)
+    println!("{:?}", macro_vector);
     let mut bob = User::new(String::from("Bob"), 32, 155.2);
     println!("{:?}", bob);
 
