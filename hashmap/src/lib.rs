6f5c80068869097491e773dbd2aea9c8a116d2c3
Kris <kristian_dilov@abv.bg>
2022-06-02 13:08:19 +0100

    added lib

--- hashmap/src/lib.rs
+++ hashmap/src/lib.rs
@@ -1,62 +1,63 @@
-mod lib;
 use std::collections::HashMap;
 
+// use Rust_workshop::part1 as p1;
 
 use std::hash::{Hasher, Hash};
 
 
-fn main() {
 
-    const INITIAL_BUCKET_COUNT: usize = 16; //Number of slots/buckets
-    type Slot<Key, Value> = Option<((Key, Value), usize)>;
+    // try out the traits and generics in rust (chapter 10)
 
-    pub struct MyHashMap<Key: Hash + Eq, Value> {
-        slots: Vec<Slot<Key, Value>>,
-        slot_count: usize,
-        item_count: usize,
+// Build a HashMap from scratch
+
+const INITIAL_BUCKET_COUNT: usize = 16; //Number of slots/buckets
+type Slot<Key, Value> = Option<((Key, Value), usize)>;
+
+
+#[derive(Debug)]
+pub struct MyHashMap<Key: Hash + Eq, Value> {
+    slots: Vec<Slot<Key, Value>>,
+    slot_count: usize,
+    item_count: usize,
+}
+
+
+
+impl<Key, Value> MyHashMap<Key, Value> 
+    where Key: Hash + Eq,
+            
+{
+
+    pub fn new() -> Self {  
+        Self { slots: Self::create_slots(INITIAL_BUCKET_COUNT), slot_count: INITIAL_BUCKET_COUNT, item_count: 0}
     }
 
-    
-
-    impl<Key, Value> MyHashMap<Key, Value> 
-        where Key: Hash + Eq,
-              
-    {
-
-        pub fn new() -> Self {  
-            Self { slots: Self::create_slots(INITIAL_BUCKET_COUNT), slot_count: INITIAL_BUCKET_COUNT, item_count: 0}
+    fn create_slots(slot_count: usize) -> Vec<Slot<Key, Value>>  {
+        let mut new_slots = Vec::with_capacity(slot_count);
+        for _ in 0..slot_count {
+            new_slots.push(None);
         }
-
-        fn create_slots(slot_count: usize) -> Vec<Slot<Key, Value>>  {
-            let mut new_slots = Vec::with_capacity(slot_count);
-            for _ in 0..slot_count {
-                new_slots.push(None);
-            }
-            new_slots
-        }
-
-
-
+        new_slots
     }
+
+
 
 }
+
+
 
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use lib::MyHashMap;
     #[test]
     fn it_works(){
         
         // create a new HashMap
         let mut map = HashMap::new();
 
-        let mut mymap = MyHashMap::new();
-
-        // test the map has been created
-        // assert_eq!(mymap, 0);
+        
 
         // insert key/value pairs into the HashMap
         assert_eq!(map.insert("foo", "bar"), None);
---
+++ target/debug/deps/hashmap-2736a718c3b9307f.d
@@ -0,0 +1,6 @@
+/Users/kristiyandilov/Rust-practise/hello-rust/target/debug/deps/hashmap-2736a718c3b9307f.rmeta: hashmap/src/main.rs hashmap/src/lib.rs
+
+/Users/kristiyandilov/Rust-practise/hello-rust/target/debug/deps/hashmap-2736a718c3b9307f.d: hashmap/src/main.rs hashmap/src/lib.rs
+
+hashmap/src/main.rs:
+hashmap/src/lib.rs:
