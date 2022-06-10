a6bea6285a55df6385b9152d59b64b6c8d3b5bb3
Kris <kristian_dilov@abv.bg>
2022-06-10 22:32:45 +0100

    solution

--- codewars/src/main.rs
+++ codewars/src/main.rs
@@ -1,15 +1,23 @@
 
 fn digitize(n: u64) -> Vec<u8> {
     
-    let mut digits = Vec::new();
-
-    
-
+    let mut digits: Vec<u8>= Vec::new();
+    let mut n = n;
+    println!("{}", n);
+    while n > 9 {
+        let x = n % 10;
+        let mut m = x as u8;
+        digits.push(m);
+        n = n/ 10
+    }
+    // digits.push(n);
+    let n = n as u8;
+    digits.push(n);
+    digits
  
-
 }
 
 fn main() {
-    let x = 9 % 10;
-    println!("{}",x);
+    println!("{:?}", digitize(35231));
+    
 }
