cca6c7753f5bed0ff0640eecd4c40535acf76fd7
Kris <kristian_dilov@abv.bg>
2022-07-01 21:23:49 +0100

    solution

--- codewars/src/main.rs
+++ codewars/src/main.rs
@@ -47,15 +47,22 @@ fn how_much_i_love_you(mut nb_petals: u16) -> &'static str {
 }
 
 fn boolean_to_string(b: bool) -> String {
-
+    if b {
+        return String::from("true");
+    }
+    else {
+        return String::from("false")
+    }
 }
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn example() {
+    fn example_stuff() {
         assert_eq!(boolean_to_string(true), "true", "When we pass in true, we want the string \"true\" as output");
         assert_eq!(boolean_to_string(false), "false", "When we pass in false, we want the string \"false\" as output");
         assert_eq!(boolean_to_string(false), "false", "When we pass in false, we want the string \"false\" as output");
