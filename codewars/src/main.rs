714edc6e201060d2d45fc352f0cbf59ad12f84d5
Kris <kristian_dilov@abv.bg>
2022-08-04 09:35:54 +0300

    validate_pin

--- codewars/src/main.rs
+++ codewars/src/main.rs
@@ -145,9 +145,35 @@ fn validate_pin(pin: &str) -> bool {
     }
 }
 
+
+
+fn open_or_senior(data: Vec<(i32, i32)>)-> Vec<String>  {
+
+    let mut output: Vec<String> = Vec::new();
+    
+
+    for batch in data.iter() {
+        println!("{:?}", batch.0);
+        if batch.0 >= 55 && batch.1 > 7 {
+            output.push("Senior".to_string())
+        }
+        else {
+            output.push("Open".to_string())
+        }
+    }
+    return output
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(open_or_senior(vec![(45, 12), (55,21), (19, -2), (104, 20)]), vec!["Open", "Senior", "Open", "Senior"]);
+        assert_eq!(open_or_senior(vec![(3, 12), (55,1), (91, -2), (54, 23)]), vec!["Open", "Open", "Open", "Open"]);
+    }
 
     #[test]
     fn invalid_length_tests() {
