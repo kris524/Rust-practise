af53fe999d7ecb04df03dd4a48079d9348829bcd
Kris <kristian_dilov@abv.bg>
2022-08-20 23:51:50 +0300

    initial commit

--- codewars/src/main.rs
+++ codewars/src/main.rs
@@ -200,19 +200,20 @@ fn arr(n: usize) -> Vec<u32> {
     for i in 0..n {
         vec1.push(i as u32)
     }
-    return vec1
+    return vec1;
 }
 
-fn bubble_sort(mut arr: Vec<i32>) -> Vec<i32>{
+fn bubble_sort(mut arr: Vec<i32>) -> Vec<i32> {
     // for j in range(0, len(array)):
 
-    for i in 0..arr.len() { //0..2
-        for j in 0..arr.len() - i -1 {
+    for i in 0..arr.len() {
+        //0..2
+        for j in 0..arr.len() - i - 1 {
             println!("arr_j = {}", arr[j]);
-            println!("arr_j+1 = {}", arr[j+1]);
+            println!("arr_j+1 = {}", arr[j + 1]);
             //a smarter way
-            if arr[j] > arr[j+1] {
-            arr.swap(j, j+1);
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
             }
             //my way
             // if arr[j] > arr[j+1] {
@@ -232,103 +233,102 @@ mod tests {
     #[test]
     fn arr_test() {
         assert_eq!(arr(0), vec![]);
-        assert_eq!(arr(4), vec![0,1,2,3]);
+        assert_eq!(arr(4), vec![0, 1, 2, 3]);
     }
 }
 
-    #[test]
-    fn test_bubble_sort() {         //[2,6,3,10]
-        assert_eq!(bubble_sort(vec![10, 2, 6, 3]), vec![2,3,6,10])
-    }
-
-    #[test]
-    fn returns_expected() {
-        assert_eq!(
-            open_or_senior(vec![(45, 12), (55, 21), (19, -2), (104, 20)]),
-            vec!["Open", "Senior", "Open", "Senior"]
-        );
-        assert_eq!(
-            open_or_senior(vec![(3, 12), (55, 1), (91, -2), (54, 23)]),
-            vec!["Open", "Open", "Open", "Open"]
-        );
-    }
-
-    #[test]
-    fn invalid_length_tests() {
-        assert_eq!(validate_pin("1"), false);
-        assert_eq!(validate_pin("12"), false);
-        assert_eq!(validate_pin("123"), false);
-        assert_eq!(validate_pin("12345"), false);
-        assert_eq!(validate_pin("1234567"), false);
-        assert_eq!(validate_pin("-1234"), false);
-        assert_eq!(validate_pin("1.234"), false);
-        assert_eq!(validate_pin("-1.234"), false);
-        assert_eq!(validate_pin("00000000"), false);
-    }
-    #[test]
-    fn non_digit_chars_tests() {
-        assert_eq!(validate_pin("a234"), false);
-        assert_eq!(validate_pin(".234"), false);
-    }
-
-    #[test]
-    fn valid_pin_tests() {
-        assert_eq!(validate_pin("1234"), true);
-        assert_eq!(validate_pin("0000"), true);
-        assert_eq!(validate_pin("1111"), true);
-        assert_eq!(validate_pin("123456"), true);
-        assert_eq!(validate_pin("098765"), true);
-        assert_eq!(validate_pin("000000"), true);
-        assert_eq!(validate_pin("123456"), true);
-        assert_eq!(validate_pin("090909"), true);
-    }
-
-    #[test]
-    fn example_stuff() {
-        assert_eq!(
-            boolean_to_string(true),
-            "true",
-            "When we pass in true, we want the string \"true\" as output"
-        );
-        assert_eq!(
-            boolean_to_string(false),
-            "false",
-            "When we pass in false, we want the string \"false\" as output"
-        );
-        assert_eq!(
-            boolean_to_string(false),
-            "false",
-            "When we pass in false, we want the string \"false\" as output"
-        );
-    }
-
-    #[test]
-    fn some_examples() {
-        assert_eq!(positive_sum(&[1, 2, 3, 4, 5]), 15);
-        assert_eq!(positive_sum(&[1, -2, 3, 4, 5]), 13);
-        assert_eq!(positive_sum(&[-1, 2, 3, 4, -5]), 9);
-    }
-
-    #[test]
-    fn empty_list() {
-        assert_eq!(positive_sum(&[]), 0);
-    }
-
-    #[test]
-    fn all_negative() {
-        assert_eq!(positive_sum(&[-1, -2, -3, -4, -5]), 0);
-    }
-
-    #[test]
-    fn test_make_upper_case() {
-        assert_eq!(make_upper_case("hello"), "HELLO");
-    }
-
-    #[test]
-    fn basic_tests() {
-        assert_eq!(bmi(50, 1.80), "Underweight");
-        assert_eq!(bmi(80, 1.80), "Normal");
-        assert_eq!(bmi(90, 1.80), "Overweight");
-        assert_eq!(bmi(110, 1.80), "Obese");
+#[test]
+fn test_bubble_sort() {
+    assert_eq!(bubble_sort(vec![10, 2, 6, 3]), vec![2, 3, 6, 10])
 }
 
+#[test]
+fn returns_expected() {
+    assert_eq!(
+        open_or_senior(vec![(45, 12), (55, 21), (19, -2), (104, 20)]),
+        vec!["Open", "Senior", "Open", "Senior"]
+    );
+    assert_eq!(
+        open_or_senior(vec![(3, 12), (55, 1), (91, -2), (54, 23)]),
+        vec!["Open", "Open", "Open", "Open"]
+    );
+}
+
+#[test]
+fn invalid_length_tests() {
+    assert_eq!(validate_pin("1"), false);
+    assert_eq!(validate_pin("12"), false);
+    assert_eq!(validate_pin("123"), false);
+    assert_eq!(validate_pin("12345"), false);
+    assert_eq!(validate_pin("1234567"), false);
+    assert_eq!(validate_pin("-1234"), false);
+    assert_eq!(validate_pin("1.234"), false);
+    assert_eq!(validate_pin("-1.234"), false);
+    assert_eq!(validate_pin("00000000"), false);
+}
+#[test]
+fn non_digit_chars_tests() {
+    assert_eq!(validate_pin("a234"), false);
+    assert_eq!(validate_pin(".234"), false);
+}
+
+#[test]
+fn valid_pin_tests() {
+    assert_eq!(validate_pin("1234"), true);
+    assert_eq!(validate_pin("0000"), true);
+    assert_eq!(validate_pin("1111"), true);
+    assert_eq!(validate_pin("123456"), true);
+    assert_eq!(validate_pin("098765"), true);
+    assert_eq!(validate_pin("000000"), true);
+    assert_eq!(validate_pin("123456"), true);
+    assert_eq!(validate_pin("090909"), true);
+}
+
+#[test]
+fn example_stuff() {
+    assert_eq!(
+        boolean_to_string(true),
+        "true",
+        "When we pass in true, we want the string \"true\" as output"
+    );
+    assert_eq!(
+        boolean_to_string(false),
+        "false",
+        "When we pass in false, we want the string \"false\" as output"
+    );
+    assert_eq!(
+        boolean_to_string(false),
+        "false",
+        "When we pass in false, we want the string \"false\" as output"
+    );
+}
+
+#[test]
+fn some_examples() {
+    assert_eq!(positive_sum(&[1, 2, 3, 4, 5]), 15);
+    assert_eq!(positive_sum(&[1, -2, 3, 4, 5]), 13);
+    assert_eq!(positive_sum(&[-1, 2, 3, 4, -5]), 9);
+}
+
+#[test]
+fn empty_list() {
+    assert_eq!(positive_sum(&[]), 0);
+}
+
+#[test]
+fn all_negative() {
+    assert_eq!(positive_sum(&[-1, -2, -3, -4, -5]), 0);
+}
+
+#[test]
+fn test_make_upper_case() {
+    assert_eq!(make_upper_case("hello"), "HELLO");
+}
+
+#[test]
+fn basic_tests() {
+    assert_eq!(bmi(50, 1.80), "Underweight");
+    assert_eq!(bmi(80, 1.80), "Normal");
+    assert_eq!(bmi(90, 1.80), "Overweight");
+    assert_eq!(bmi(110, 1.80), "Obese");
+}
