a1ec52f7f4e10953e14f70c7ff8901013aa9f4d6
Kris <kristian_dilov@abv.bg>
2022-08-23 22:25:40 +0300

    initial commit

---
+++ concurrency/src/bin/another-concurrency.rs
@@ -0,0 +1,27 @@
+use std::{thread, time::Duration};
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("The number we are at the spawned thread {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    // handle.join().unwrap();
+    //if the main thread ends, the spawn thread stops
+    for i in 1..5 {
+        println!("The number we are at the main thread {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    //that is why we use a handle to block the thread currently running (main thread)
+    //until the thread asociated with the handle (the spawn thread) terminates
+    handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let handle2 = thread::spawn(|| {
+        println!("Heres a vector")
+    });
+}
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,22 +1,42 @@
 use std::thread;
 
+//an object that is used to ensure Mutual Exclusion -> only one thread has access to a resource at any given time
+use std::sync::{Mutex, Arc};
+
 fn main() {
+    //create the mutex with integer value 0 as the controlled resource
+    let counter = Arc::new(Mutex::new(0));
+
 
     //create a vector that tracks all the handles to threads that we create so we can close them later
     let mut handles = vec![];
 
+    //spawn 10 threads
     for _ in 0..10 {
+
+        let counter = Arc::clone(&counter);
+        //spawn a thread using the library
         let handle = thread::spawn( move || {
-                println!("HELLO!");
+
+            //
+            let mut num = counter.lock().unwrap();
+            *num += 1
+                
             }
         );
+        //add the newly created handle into the list of handles
         handles.push(handle)
     }
 
+    //call join on all the handles and wait for them to finish executing 
     for handle in handles {
         handle.join().unwrap();
     }
+    println!("RESULT: {}", *counter.lock().unwrap());
     return;
 
 
 }
+
+//In multithreded rust programing, we cant use a mutex on its own, 
+//We need to wrap the mutex using a reference structure to be passed by value into out movement closure 
